b0cceab6be3c11bba2e572d642b7ea50aca318fa
reedwane <reedart07@gmail.com>
2021-11-09 10:17:29 +0100

    conflicts 1.1

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { DataContext } from "./Context/dataContext";
@@ -27,21 +26,6 @@ const Home = () => {
 					<Link className='circle explore' to='destinations/Moon'>Explore</Link>
 				</div>
 			}
-=======
-import Content from "./Content";
-const Home = () => {
-	return (
-		<div>
-			<h1>Home</h1>
-			<Content />
-			<p>
-				So, you want to travel to Space Let’s face it; if you want to go to
-				space, you might as well genuinely go to outer space and not hover kind
-				of on the edge of it. Well sit back, and relax because we’ll give you a
-				truly out of this world experience!
-				<br /> Explore
-			</p>
->>>>>>> 7c08dbb6db610df77da20c7c0cc7f05e0942d4e3
 		</div>
 	);
 };
